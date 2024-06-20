f8158167a1805df63c0655ec1e5adc6cd2eac3e6
Hitesh sharma <sharmahitesh9828@gmail.com>
2024-06-20 23:29:17 +0530

    karkhana

---
+++ karkhana.io/assignment/README.md
@@ -0,0 +1,58 @@
+
+
+# Gimber E-Commerce Application
+
+<h3>book your order at just one click</h3>
+
+This is a responsive e-commerce application built with React that allows users to browse various Gimber products, add them to a cart, and view the cart summary. The application includes user-friendly features and a visually appealing interface.
+
+## Features
+Product Listing: Browse through a variety of Gimber products.
+Search Functionality: Search for products using the search bar.
+Category Filters: Filter products by categories like Ice Coffee, Hot Coffee, Artisan Tea, Ice Mojito, and Beverage.
+Add to Cart: Add products to the cart with a single click.
+Cart Summary: View the cart summary with detailed pricing, discounts, and total amount.
+
+
+  Deploy Link -
+  Netlify: https://66746d36f310b69a1450e5aa--willowy-crepe-839d42.netlify.app/
+
+
+---
+
+## Tech Stack Used: -
+
+- #### Languages
+  - `HTML`
+  - `CSS`
+  - `JavaScript `
+  - `React`
+  
+---
+## Running it locally
+
+- run `npm install` or `yarn install`
+- run `npm run dev`
+
+
+### Screenshots :camera:
+
+---
+- **Home Page**
+---
+![Gimber E-Commerce](./src/assets/ss1.png)
+![1 2](./src/assets/ss2.png
+)
+---
+
+
+
+
+
+
+### About Project
+
+> This is a responsive e-commerce application built with React that allows users to browse various Gimber products, add them to a cart, and view the cart summary. The application includes user-friendly features and a visually appealing interface.
+
+---
+
---
+++ karkhana.io/assignment/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState,useEffect } from 'react'
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import './App.css'
+
+const products = [
+  { id: 1,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'ORI GIMBER 700ml', price: 24.95 },
+  { id: 2,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'GIMBER N°2 700 ml', price: 25.85 },
+  { id: 3,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'SML GIMBER 500ml', price: 20.45 },
+  { id: 4,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'GIMBER N°2 500 ml', price: 26.00 },
+  { id: 5,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'GIMBER N°2 500 ml', price: 26.00 },
+  { id: 6,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'GIMBER N°2 500 ml', price: 26.00 },
+  { id: 7,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'GIMBER N°2 500 ml', price: 26.00 },
+  { id: 8,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'GIMBER N°2 500 ml', price: 26.00 },
+  { id: 9,img:"https://uk.gimber.com/cdn/shop/products/GIMBER-500ml_1500x1500_crop_center.jpg?v=1608712713", name: 'GIMBER N°2 500 ml', price: 26.00 },
+  // Add other products as needed
+];
+
+
+function App() {
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+    setTotal(total + product.price);
+  };
+
+
+  useEffect(() => {
+    const newTotal = cart.reduce((sum, item) => sum + item.price, 0);
+    setTotal(newTotal);
+  }, [cart]);
+
+
+  const handleDelete = (index) => {
+    const newCart = [...cart];
+    newCart.splice(index, 1);
+    setCart(newCart);
+
+  };
+
+  return (
+    <>
+    <div className="App">
+    <div className="product-list">
+      <ProductList products={products} addToCart={addToCart} />
+    </div>
+    <div className="cart">
+      <Cart cart={cart} total={total} handleDelete={handleDelete} />
+    </div>
+   </div>
+    <div className='navbar' style={{border:"1px solid black",display:"flex",justifyContent:"space-around"}}>
+    < i className='swing-fwd-center' class='bx bxs-grid-alt'></i><i class='bx bxs-circle'></i><i class='bx bxs-receipt'></i><i class='bx bxl-google' ></i>
+      </div></>
+  )
+}
+
+export default App
