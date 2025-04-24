74f452f47bf330389e9d0061bb26463fc2b91e93
Hitesh-sharma-karkhana <hitesh@karkhana.io>
2025-04-24 14:15:56 +0530

    quantum

---
+++ Quantum it/server/Middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt=require('jsonwebtoken')
+
+const auth=(req,res,next)=>{
+    const token =req.headers.authorization
+ try {
+    if(token){
+    jwt.verify(token,"user",function(err,decode){
+        if(decode){
+            next()
+        }else{
+            res.send({msg:"session expired login again"})
+        }
+    })
+}
+else{
+    res.send({msg:"need to login"})
+}
+ } catch (error) {
+    res.send({"err":error})
+ }
+
+}
+
+module.exports={auth}
